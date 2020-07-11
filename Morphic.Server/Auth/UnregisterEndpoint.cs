a8fee067242313fd69eda883a43194088b5f15a0
Jan Vilhuber <jan@vilhuber.com>
2020-07-11 17:45:50 +0700

    revise unregister endpoint simply authenticated for now.

--- Morphic.Server/Auth/UnregisterEndpoint.cs
+++ Morphic.Server/Auth/UnregisterEndpoint.cs
@@ -21,101 +21,50 @@
 // * Adobe Foundation
 // * Consumer Electronics Association Foundation
 
+using System.Net;
 using System.Threading.Tasks;
-using System.Text.Json.Serialization;
+using Hangfire;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Serilog;
 
 namespace Morphic.Server.Auth
 {
     using Http;
     using Users;
-    using Db;
-
-    public abstract class UnregisterEndpoint : Endpoint
-    {
-        protected async Task Unregister<TCredential>(User user, TCredential cred) where TCredential : Record
-        {
-            var db = Context.GetDatabase();
-            var deleted = await db.DeleteMany<Preferences>(r => r.UserId == user.Id);
-            Log.Logger.Debug($"Deleted {deleted} Preferences");
-            deleted = await db.DeleteMany<AuthToken>(r => r.UserId == user.Id);
-            Log.Logger.Debug($"Deleted {deleted} AuthTokens");
-            deleted = await db.DeleteMany<BadPasswordLockout>(r => r.Id == user.Id);
-            Log.Logger.Debug($"Deleted {deleted} AuthTokens");
-            await Delete(cred);
-            await Delete(user);
-        }
-
-        protected UnregisterEndpoint(IHttpContextAccessor contextAccessor, ILogger<Endpoint> logger) : base(contextAccessor, logger)
-        {
-        }
-    }
     
-    public class UnregisterUsernameRequest
-    {
-        [JsonPropertyName("username")]
-        public string Username { get; set; } = "";
-
-        [JsonPropertyName("password")]
-        public string Password { get; set; } = "";
-    }
-
-    public class UnregisterKeyRequest
-    {
-        [JsonPropertyName("key")]
-        public string Key { get; set; } = "";
-    }
-
-
     /// <summary>
-    /// Endpoint Controller for username unregister requests. Security relies on the username-password
-    /// being passed in the request. The reason is two-fold: 1) the user may decide to unregister separately
-    /// from using the product (and perhaps not having logge in in a while), so it seems weird to ask them
-    /// to log in first, and then unregister (though perhaps that's not so unreasonable), and 2) since this
-    /// is a destructive action we don't want someone who hijacks the auth-token to be able to do this.
-    ///
-    /// TODO We could make this require the auth token, but I would still recommend we make the user enter their password for extra security anyway
+    /// Endpoint Controller for unregister requests.
     /// </summary>
-    [Path("/v1/unregister/username")]
-    public class UnregisterUsernameEndpoint : UnregisterEndpoint
+    [Path("/v1/users/{UserId}/unregister")]
+    public class UnregisterUsernameEndpoint : Endpoint
     {
+        private readonly IBackgroundJobClient jobClient;
 
+        public UnregisterUsernameEndpoint(
+            IHttpContextAccessor contextAccessor,
+            ILogger<ChangePasswordEndpoint> logger,
+            IBackgroundJobClient jobClient): base(contextAccessor, logger)
+        {
+            this.jobClient = jobClient;
+        }
+
+        /// <summary>The lookup id to use, populated from the request URL</summary>
+        [Parameter]
+        public string UserId = "";
+        
+        public override async Task LoadResource()
+        {
+            var authenticatedUser = await RequireUser();
+            if (authenticatedUser.Id != UserId)
+            {
+                throw new HttpError(HttpStatusCode.Forbidden);
+            }
+        }
+        
         [Method]
         public async Task Post()
         {
-            var request = await Request.ReadJson<UnregisterUsernameRequest>();
-            var db = Context.GetDatabase();
-            var user = await db.UserForUsername(request.Username, request.Password);
-            var cred = await Load<UsernameCredential>(request.Username);
-            await Unregister(user, cred);
-        }
-
-        public UnregisterUsernameEndpoint(IHttpContextAccessor contextAccessor, ILogger<Endpoint> logger) : base(contextAccessor, logger)
-        {
+            await Task.Run(() => { jobClient.Enqueue<UserCleanupJob>(x => x.UnregisterUser(UserId)); });
         }
     }
-    
-    /// <summary>Endpoint Controller for auth-key unregister requests</summary>
-    // Disabling until we have a legitimate use case.  Not removing because we expect
-    // to re-enable at some point down the line for something like a USB stick login.
-    //[Path("/v1/unregister/key")]
-    public class UnregisterKeyEndpoint : UnregisterEndpoint
-    {
-        [Method]
-        public async Task Post()
-        {
-            var request = await Request.ReadJson<UnregisterKeyRequest>();
-            var db = Context.GetDatabase();
-            var user = await db.UserForKey(request.Key);
-            var cred = await Load<UsernameCredential>(request.Key);
-            await Unregister(user, cred);
-        }
-
-        public UnregisterKeyEndpoint(IHttpContextAccessor contextAccessor, ILogger<Endpoint> logger) : base(contextAccessor, logger)
-        {
-        }
-    }
-
 }
